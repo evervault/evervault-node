272b4542bc7d5861642622cca83deac2308992ad
Liam <liam@evervault.com>
2020-06-23 12:49:38 +0100

    create test file

---
+++ tests/core/crypto.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const Crypto = require('../../lib/core/crypto');
+
+describe('Crypto Module', () => {
+  it('works', () => {
+    expect(true).to.be.true;
+  });
+});
