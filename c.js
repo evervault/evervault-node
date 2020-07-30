7d4a913d7f8b7c2497db39504a2cf0de894fbb33
Shane Curran <shane@evervault.com>
2020-07-30 17:42:14 +0100

    Removed junk

--- c.js
+++
@@ -1,9 +0,0 @@
-const ev = require('.');
-
-const e = new ev('MTE=:WzWnwo4uNnWcb7HLG6M0zboFJiD956nixaXbXth+Zw0=');
-
-const magic = e.cagify((a, b) => Math.pow(a,b));
-
-(async () => {
-    console.log(await magic(2, 8));
-})()
