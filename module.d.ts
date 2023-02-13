667ced8814a91d6c6378b860c847c1d4d1c838f7
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2023-02-13 14:25:17 +0000

    refactor(module.d.ts): npm module declaration file to help with ts
    i wanted to use evervault with ts but couldnt due to missing types. so, i made a declaration file. i
    dont know if it works when you try to import the module upon installation from npm but i run a few
    quick tests locally by importing it directly from the fs and it went smoothly

---
+++ module.d.ts
@@ -0,0 +1,10 @@
+declare module "@evervault/sdk" {
+    export default class Evervault {
+        constructor(apiKey: string)
+        encrypt: (data: string) => Promise<string>;
+        run: <T>(functionName: string, data: object, options?: { async?: boolean, version?: string }) => Promise<{ result: T, runId: string, appUuid: string }>;
+        createRunToken: (functionName: string, data: object) => Promise<{ token: string }>;
+        enableOutboundRelay: (options: { decryptionDomains: string[], debugRequests: boolean }) => Promise<void>;
+        enableCagesBeta: (cageAttestationData: { prc0?: string, prc1?: string, prc2?: string, prc8?: string }) => Promise<void>;
+    }
+}
