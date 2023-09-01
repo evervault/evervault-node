47b0bebf3f5fb31116b962a00af4a5811904d0cd
Jake Grogan <jake.kgrogan@gmail.com>
2023-09-01 14:17:14 +0100

    chore(e2e_tests): lint

--- e2e/encryptDecrypt.test.js
+++ e2e/encryptDecrypt.test.js
@@ -1,71 +1,71 @@
 const { expect } = require('chai');
-const Evervault = require('../lib');;
+const Evervault = require('../lib');
 
-describe("Encrypt and Decrypt", () => {
-    const appUuid = process.env.EV_APP_UUID;
-    const apiKey = process.env.EV_API_KEY;
+describe('Encrypt and Decrypt', () => {
+  const appUuid = process.env.EV_APP_UUID;
+  const apiKey = process.env.EV_API_KEY;
 
-    let evervaultClient;
-    
-    beforeEach(() => {
-        evervaultClient = new Evervault(appUuid, apiKey);
+  let evervaultClient;
+
+  beforeEach(() => {
+    evervaultClient = new Evervault(appUuid, apiKey);
+  });
+
+  context('Encrypt and Decrypt', () => {
+    it('encrypts boolean true', async () => {
+      const payload = true;
+      const encrypted = await evervaultClient.encrypt(payload);
+      const decrypted = await evervaultClient.decrypt(encrypted);
+      expect(payload).to.equal(decrypted);
     });
 
-    context('Encrypt and Decrypt', () => {
-        it("encrypts boolean true", async () => {
-            const payload = true;
-            const encrypted = await evervaultClient.encrypt(payload);
-            const decrypted = await evervaultClient.decrypt(encrypted);
-            expect(payload).to.equal(decrypted);
-        });
-
-        it('Encrypt boolean false', async () => {
-            const payload = false;
-            const encrypted = await evervaultClient.encrypt(payload);
-            const decrypted = await evervaultClient.decrypt(encrypted);
-            expect(payload).to.equal(decrypted);
-        });
-
-        it('Encrypt string', async () => {
-            const payload = "hello world";
-            const encrypted = await evervaultClient.encrypt(payload);
-            const decrypted = await evervaultClient.decrypt(encrypted);
-            expect(payload).to.equal(decrypted);
-        });
-
-        it('Encrypt integer', async () => {
-            const payload = 1;
-            const encrypted = await evervaultClient.encrypt(payload);
-            const decrypted = await evervaultClient.decrypt(encrypted);
-            expect(payload).to.equal(decrypted);
-        });
-
-        it('Encrypt float', async () => {
-            const payload = 1.5;
-            const encrypted = await evervaultClient.encrypt(payload);
-            const decrypted = await evervaultClient.decrypt(encrypted);
-            expect(payload).to.equal(decrypted);
-        });
-
-        it('Encrypt array', async () => {
-            const payload = ["hello", 1, 1.5, true, false];
-            const encrypted = await evervaultClient.encrypt(payload);
-            const decrypted = await evervaultClient.decrypt(encrypted);
-            expect(payload).to.deep.equal(decrypted);
-        });
-
-        it('Encrypt object', async () => {
-            const payload = {
-                "string": "hello",
-                "integer": 1,
-                "float": 1.5,
-                "true": true,
-                "false": false,
-                "array": ["hello", 1, 1.5, true, false]
-            };
-            const encrypted = await evervaultClient.encrypt(payload);
-            const decrypted = await evervaultClient.decrypt(encrypted);
-            expect(payload).to.deep.equal(decrypted);
-        });
+    it('Encrypt boolean false', async () => {
+      const payload = false;
+      const encrypted = await evervaultClient.encrypt(payload);
+      const decrypted = await evervaultClient.decrypt(encrypted);
+      expect(payload).to.equal(decrypted);
     });
+
+    it('Encrypt string', async () => {
+      const payload = 'hello world';
+      const encrypted = await evervaultClient.encrypt(payload);
+      const decrypted = await evervaultClient.decrypt(encrypted);
+      expect(payload).to.equal(decrypted);
+    });
+
+    it('Encrypt integer', async () => {
+      const payload = 1;
+      const encrypted = await evervaultClient.encrypt(payload);
+      const decrypted = await evervaultClient.decrypt(encrypted);
+      expect(payload).to.equal(decrypted);
+    });
+
+    it('Encrypt float', async () => {
+      const payload = 1.5;
+      const encrypted = await evervaultClient.encrypt(payload);
+      const decrypted = await evervaultClient.decrypt(encrypted);
+      expect(payload).to.equal(decrypted);
+    });
+
+    it('Encrypt array', async () => {
+      const payload = ['hello', 1, 1.5, true, false];
+      const encrypted = await evervaultClient.encrypt(payload);
+      const decrypted = await evervaultClient.decrypt(encrypted);
+      expect(payload).to.deep.equal(decrypted);
+    });
+
+    it('Encrypt object', async () => {
+      const payload = {
+        string: 'hello',
+        integer: 1,
+        float: 1.5,
+        true: true,
+        false: false,
+        array: ['hello', 1, 1.5, true, false],
+      };
+      const encrypted = await evervaultClient.encrypt(payload);
+      const decrypted = await evervaultClient.decrypt(encrypted);
+      expect(payload).to.deep.equal(decrypted);
+    });
+  });
 });
