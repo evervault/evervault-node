bf5d6b35020a96b6b90a50639d2c11800b2cce15
Eoin Boylan <jb.eoin@gmail.com>
2023-09-06 17:27:55 +0100

    feat: add createRelayHttpsAgent to return configured https agent

--- lib/evervault.d.ts
+++ lib/evervault.d.ts
@@ -1,3 +1,4 @@
+import HttpsProxyAgent from "./utils/proxyAgent";
 declare module "@evervault/sdk" {
     export default class Evervault {
         constructor(appId: string, apiKey: string)
@@ -6,7 +7,8 @@ declare module "@evervault/sdk" {
         createClientSideDecryptToken: (payload: any, expiry?: Date) => Promise<{ id: string, token: string, createdAt: Date, expiry: Date }>;
         run: <T>(functionName: string, data: object, options?: { async?: boolean, version?: string }) => Promise<{ result: T, runId: string, appUuid: string }>;
         createRunToken: (functionName: string, data: object) => Promise<{ token: string }>;
-        enableOutboundRelay: (options: { decryptionDomains: string[], debugRequests: boolean }) => Promise<void>;
+        enableOutboundRelay: (options?: { decryptionDomains: string[], debugRequests: boolean }) => Promise<void>;
         enableCagesBeta: (cageAttestationData: Record<string, { pcr0?: string, pcr1?: string, pcr2?: string, pcr8?: string } | {pcr0?: string, pcr1?: string, pcr2?: string, pcr8?: string }[]>) => Promise<void>;
+        createRelayHttpsAgent: () => HttpsProxyAgent;
     }
 }
