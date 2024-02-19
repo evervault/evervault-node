759861a7d82b685f620ff62813f0a91d2947c64c
Liam Farrelly <liam@evervault.com>
2024-02-19 17:24:24 +0000

    Move attestation bindings in NodeSDK to be an external dependency (#172)
    * Remove deprecated functions, move attestation bindings to be explicitly passed
    
    * remove all references to cage
    
    * update attestation tests
    
    * update typings file
    
    * Remove dependency on attestation bindings, define bindings in typings file
    
    * chore: add changeset for attestation bindings moving to a plugin
    
    * Move shared types into .d.ts file

---
+++ lib/types.d.ts
@@ -0,0 +1,64 @@
+export interface HttpConfig {
+  baseUrl: string;
+  userAgent: string;
+  tunnelHostname: string;
+  certHostname: string;
+  enclavesHostname: string;
+  pollInterval: string | number;
+  attestationDocPollInterval: string | number;
+  pcrProviderPollInterval: string | number;
+}
+
+export interface CurveConfig {
+  ecdhCurve: string;
+  keyCycleMinutes: number;
+  cipherAlgorithm: string;
+  keyLength: number;
+  ivLength: number;
+  authTagLength: number;
+  evVersion: string;
+  evVersionWithMetadata: string;
+  header: { iss: string; version: number };
+  maxFileSizeInMB: string | number;
+}
+
+export type SupportedCurve = 'secp256k1' | 'prime256v1';
+
+export interface CryptoConfig {
+  secp256k1: CurveConfig;
+  prime256v1: CurveConfig;
+}
+
+export interface MasterConfig {
+  http: HttpConfig;
+  encryption: CryptoConfig;
+}
+
+export interface OutboundRelayOptions {
+  decryptionDomains?: string[];
+  debugRequests?: boolean;
+}
+
+export interface SdkOptions {
+  curve?: SupportedCurve;
+  retry?: boolean;
+  enableOutboundRelay?: boolean;
+}
+
+export interface PCRs {
+  pcr0?: string;
+  pcr1?: string;
+  pcr2?: string;
+  pcr8?: string;
+}
+
+export type AttestationData = PCRs | PCRs[];
+export type AttestationCallback = () => Promise<AttestationData>;
+
+export interface AttestationBindings {
+  attestEnclave: (
+    cert: Buffer,
+    expectedPcrsList: Array<PCRs>,
+    attestationDoc: Buffer
+  ) => boolean;
+}
--- tests/utils/attest.test.js
+++ tests/utils/attest.test.js
@@ -1,124 +1,240 @@
 const fs = require('fs');
 const path = require('path');
 const { expect } = require('chai');
-const { attest, errors } = require('../../lib/utils');
+const { attest } = require('../../lib/utils');
+const { AttestationDoc, PcrManager } = require('../../lib/core');
 const config = require('../../lib/config');
+const sinon = require('sinon');
+const {
+  AttestationError,
+  MalformedAttestationData,
+} = require('../../lib/utils/errors');
 
 /**
  * Note: these tests are time sensitive, so are expected to fail when used without libfaketime
  * To avoid false negatives, these tests will only run when the FAKETIME env var is set
  */
-const fakeTimeTests =
-  process.env.FAKETIME != null && attest.hasAttestationBindings()
-    ? describe
-    : describe.skip;
+const fakeTimeTests = process.env.FAKETIME != null ? describe : describe.skip;
 
-describe('attestBeta', () => {
-  describe('parseCageNameAndAppFromHost', () => {
-    const testCage = 'my-cage';
-    const testApp = 'my-app';
-    const hostname = 'cages.evervault.com';
-    context('Request to base cage host name', () => {
-      it('returns expected cage name', () => {
-        const cageName = attest.parseCageNameAndAppFromHost(
-          `${testCage}.${testApp}.${hostname}`
-        );
-        expect(cageName).to.deep.equal({
-          cageName: testCage,
-          appUuid: testApp,
-        });
-      });
-    });
-    context('Request to cage nonce subdomain', () => {
-      it('returns expected cage name', () => {
-        const cageName = attest.parseCageNameAndAppFromHost(
-          `noncey.attest.${testCage}.${testApp}.${hostname}`
-        );
-        expect(cageName).to.deep.equal({
-          cageName: testCage,
-          appUuid: testApp,
-        });
-      });
-    });
-  });
-
-  fakeTimeTests('attestCageConnectionBeta', () => {
+describe('attestGA', async () => {
+  fakeTimeTests('attestConnection', async () => {
     const cageName = 'a-test-of-attest';
     const appUuid = 'app_452e33b20b42';
-    const hostname = 'cages.evervault.com';
+    const hostname = 'enclave.evervault.com';
     const validPCRs = {
-      pcr0: 'd265a83faa7b4fa0d73b82b6d06253e894445922937d0ee5a74fe4891da9817611a71e71b98e5329232902de3cf419af',
-      pcr1: 'bcdf05fefccaa8e55bf2c8d6dee9e79bbff31e34bf28a99aa19e6b29c37ee80b214a414b7607236edf26fcb78654e63f',
-      pcr2: 'e4f634d24ad83b7f6e49fe283bafdb220f6252fb4a278a858e19d15b7cd0f263b9168ed0bacefb4444266df9f9a77f24',
-      pcr8: '97c5395a83c0d6a04d53ff962663c714c178c24500bf97f78456ed3721d922cf3f940614da4bb90107c439bc4a1443ca',
+      pcr0: '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+      pcr1: '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+      pcr2: '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+      pcr8: '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+    };
+    const invalidPCR = {
+      pcr8: 'Invalid0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
     };
     const derCert = fs.readFileSync(
-      path.resolve(
-        __dirname,
-        '../utilities/non-debug-cert-containing-attestation-document-18-1-23-der.crt'
-      )
+      path.resolve(__dirname, '../utilities/cage-certificate.crt')
     );
 
-    context('given a valid cert and PCRs', () => {
-      it('successfully attests the connection', () => {
-        const result = attest.attestCageConnectionBeta(
-          `${cageName}.${appUuid}.${hostname}`,
-          {
-            raw: derCert,
-          },
-          {
-            [cageName]: validPCRs,
-          }
-        );
+    const attestationDocResponse = fs.readFileSync(
+      path.resolve(__dirname, '../utilities/attestation-doc.json')
+    );
+
+    let httpStub = {
+      getAttestationDoc: sinon
+        .stub()
+        .resolves(JSON.parse(attestationDocResponse)),
+    };
+
+    afterEach(() => {
+      httpStub.getAttestationDoc.resetHistory();
+    });
+
+    context('validateAttestationData', () => {
+      it('Throws a MalformedAttestationData error if the data is not an object', () => {
+        try {
+          attest.validateAttestationData([]);
+        } catch (err) {
+          expect(err).to.be.instanceOf(MalformedAttestationData);
+        }
+      });
+
+      it('Throws a MalformedAttestationData error if the data contains non-object values', () => {
+        try {
+          attest.validateAttestationData({ 'my-cage': true });
+        } catch (err) {
+          expect(err).to.be.instanceOf(MalformedAttestationData);
+        }
+      });
+
+      it('Does not throw when the values in the object are themselves objects', () => {
+        const result = attest.validateAttestationData({ 'my-cage': {} });
         expect(result).to.be.undefined;
       });
     });
 
-    context('given a valid cert and no PCRs', () => {
-      it('successfully attests the connection', () => {
-        const result = attest.attestCageConnectionBeta(
+    context('given a valid cert and PCRs', async () => {
+      it('successfully attests the connection', async () => {
+        let cache = new AttestationDoc(config, httpStub, [cageName], appUuid);
+        await cache.init();
+        let testProvider = () => {
+          return new Promise((resolve) => {
+            resolve([validPCRs]);
+          });
+        };
+
+        let testAttestationData = { [cageName]: testProvider };
+        let manager = new PcrManager(config, testAttestationData);
+        await manager.init();
+
+        const result = await attest.attestConnection(
           `${cageName}.${appUuid}.${hostname}`,
+          derCert,
+          manager,
+          cache,
           {
-            raw: derCert,
+            attestEnclave: (_, pcrs) => {
+              expect(pcrs).to.deep.equal([validPCRs]);
+              return true;
+            },
           }
         );
         expect(result).to.be.undefined;
+        cache.disablePolling();
+        manager.disablePolling();
+      });
+    });
+
+    context(
+      'given a valid cert and PCRs with an alternative hostname',
+      async () => {
+        it('calls the cage at the alternative hostname, and  successfully attests the connection', async () => {
+          let cache = new AttestationDoc(
+            config,
+            httpStub,
+            [cageName],
+            appUuid,
+            'enclave.evervault.com'
+          );
+          await cache.init();
+          let testProvider = () => {
+            return new Promise((resolve) => {
+              resolve([validPCRs]);
+            });
+          };
+
+          let testAttestationData = { [cageName]: testProvider };
+          let manager = new PcrManager(config, testAttestationData);
+          await manager.init();
+
+          const result = await attest.attestConnection(
+            `${cageName}.${appUuid}.${hostname}`,
+            derCert,
+            manager,
+            cache,
+            {
+              attestEnclave: (_, pcrs) => {
+                expect(pcrs).to.deep.equal([validPCRs]);
+                return true;
+              },
+            }
+          );
+          expect(result).to.be.undefined;
+          expect(httpStub.getAttestationDoc).to.have.been.calledWith(
+            cageName,
+            appUuid.replace('_', '-'),
+            'enclave.evervault.com'
+          );
+          cache.disablePolling();
+          manager.disablePolling();
+        });
+      }
+    );
+
+    context('given a valid cert and no PCRs', async () => {
+      it('successfully attests the connection', async () => {
+        let cache = new AttestationDoc(config, httpStub, [cageName], appUuid);
+        await cache.init();
+        let testProvider = () => {
+          return new Promise((resolve) => {
+            resolve([validPCRs]);
+          });
+        };
+
+        let testAttestationData = { [cageName]: testProvider };
+        let manager = new PcrManager(config, testAttestationData);
+        await manager.init();
+
+        const result = await attest.attestConnection(
+          `${cageName}.${appUuid}.${hostname}`,
+          derCert,
+          manager,
+          cache,
+          {
+            attestEnclave: (_, pcrs) => {
+              expect(pcrs).to.deep.equal([validPCRs]);
+              return true;
+            },
+          }
+        );
+        expect(result).to.be.undefined;
+        cache.disablePolling();
+        manager.disablePolling();
       });
     });
 
     context('given a valid cert and some PCRs', () => {
-      it('successfully attests the connection', () => {
-        const result = attest.attestCageConnectionBeta(
+      it('successfully attests the connection', async () => {
+        let cache = new AttestationDoc(config, httpStub, [cageName], appUuid);
+        await cache.init();
+
+        let testAttestationData = { [cageName]: validPCRs };
+        let manager = new PcrManager(config, testAttestationData);
+        await manager.init();
+
+        const result = await attest.attestConnection(
           `${cageName}.${appUuid}.${hostname}`,
+          derCert,
+          manager,
+          cache,
           {
-            raw: derCert,
-          },
-          {
-            [cageName]: {
-              pcr8: validPCRs.pcr8,
+            attestEnclave: (_, pcrs) => {
+              expect(pcrs).to.deep.equal([validPCRs]);
+              return true;
             },
           }
         );
         expect(result).to.be.undefined;
+        cache.disablePolling();
+        manager.disablePolling();
       });
     });
 
     context('given a valid cert with incorrect PCRs', () => {
-      it('rejects the connection with an attestation error', () => {
+      it('rejects the connection with an attestation error', async () => {
+        let cache, manager;
         try {
-          const invalidPCRs = { ...validPCRs };
-          invalidPCRs.pcr8 = validPCRs.pcr0;
-          attest.attestCageConnectionBeta(
+          cache = new AttestationDoc(config, httpStub, [cageName], appUuid);
+          await cache.init();
+          let testAttestationData = { [cageName]: invalidPCR };
+          manager = new PcrManager(config, testAttestationData);
+          await manager.init();
+
+          await attest.attestConnection(
             `${cageName}.${appUuid}.${hostname}`,
+            derCert,
+            manager,
+            cache,
             {
-              raw: derCert,
-            },
-            {
-              [cageName]: invalidPCRs,
+              attestEnclave: (_, pcrs) => {
+                expect(pcrs).to.deep.equal([invalidPCR]);
+                return false;
+              },
             }
           );
         } catch (err) {
-          expect(err).to.be.instanceOf(errors.CageAttestationError);
+          expect(err).to.be.instanceOf(AttestationError);
+        } finally {
+          cache.disablePolling();
+          manager.disablePolling();
         }
       });
     });
