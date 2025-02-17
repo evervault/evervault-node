cdfdf1c33dedfcae92b6a169eb134d24c0b816af
Liam <liam@evervault.com>
2025-02-17 16:14:12 +0000

    chore: expand e2e tests to stress test decryption domain filtering

--- e2e/outboundRelay.test.js
+++ e2e/outboundRelay.test.js
@@ -38,4 +38,79 @@ describe('Outbound Relay Test', () => {
       expect(response.request[proxiedMarker]).to.equal(true);
     });
   });
+
+  context('Enable Outbound Relay with decryption domains', () => {
+    it('Proxies the listed domains correctly, ignoring others', async () => {
+      const payload = {
+        string: 'some_string',
+        number: 1234567890,
+        boolean: true,
+      };
+      const encrypted = await evervaultClient.encrypt(payload);
+
+      await evervaultClient.enableOutboundRelay({
+        decryptionDomains: ['httpbin.org'],
+      });
+
+      const [httpbinRes, syntheticRes] = await Promise.allSettled([
+        axios.post('https://httpbin.org/post', encrypted),
+        axios.get(syntheticEndpointUrl),
+      ]);
+
+      expect(httpbinRes.value.request[proxiedMarker]).to.equal(true);
+      expect(syntheticRes.value.request[proxiedMarker]).to.equal(false);
+    });
+  });
+
+  context(
+    'Enable Outbound Relay with decryption domains including paths',
+    () => {
+      it('Filters requests by path', async () => {
+        const payload = {
+          string: 'some_string',
+          number: 1234567890,
+          boolean: true,
+        };
+        const encrypted = await evervaultClient.encrypt(payload);
+
+        await evervaultClient.enableOutboundRelay({
+          decryptionDomains: ['httpbin.org/post'],
+        });
+
+        const [postRes, getRes] = await Promise.allSettled([
+          axios.post('https://httpbin.org/post', encrypted),
+          axios.get('https://httpbin.org/get'),
+        ]);
+
+        expect(postRes.value.request[proxiedMarker]).to.equal(true);
+        expect(getRes.value.request[proxiedMarker]).to.equal(false);
+      });
+    }
+  );
+
+  context(
+    'Enable Outbound Relay with decryption domains including paths with wildcards',
+    () => {
+      it('Filters requests by path, respecting wildcards', async () => {
+        const payload = {
+          string: 'some_string',
+          number: 1234567890,
+          boolean: true,
+        };
+        const encrypted = await evervaultClient.encrypt(payload);
+
+        await evervaultClient.enableOutboundRelay({
+          decryptionDomains: ['httpbin.org/post/*'],
+        });
+
+        const [matchingPostRes, failingPostRes] = await Promise.allSettled([
+          axios.post('https://httpbin.org/post/somewhere', encrypted),
+          axios.get('https://httpbin.org/post'),
+        ]);
+
+        expect(matchingPostRes.value.request[proxiedMarker]).to.equal(true);
+        expect(failingPostRes.value.request[proxiedMarker]).to.equal(false);
+      });
+    }
+  );
 });
